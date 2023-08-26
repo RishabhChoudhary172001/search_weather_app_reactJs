37757ae6aea9bf251bfbb0eb43add9ef1835bb45
RishabhChoudhary172001 <rishabhchoudhary172001@gmail.com>
2023-08-26 11:17:17 +0530

    This is Search weather app

--- src/components/SearchWeather.jsx
+++ src/components/SearchWeather.jsx
@@ -1,24 +1,143 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+import "./opac.css";
 
 const SearchWeather = () => {
-    return (
-        <div>
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-nd-4'>
-                        <div class="card text-bg-dark">
-                            <img src="https://source.unsplash.com/600x900/?nature,water" class="card-img" alt="..." />
-                            <div class="card-img-overlay">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small>Last updated 3 mins ago</small></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+  const [search, setSearch] = useState("London");
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState("");
+  let componentMounted = true;
 
-export default SearchWeather
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=cb23345ac3b26ba6b4b8bac501631da7`
+      );
+      if (componentMounted) {
+        setData(await response.json());
+        console.log(data);
+      }
+      return () => {
+        componentMounted = false;
+      };
+    };
+    fetchWeather();
+  }, [search]);
+
+  let emoji = null;
+  if (typeof data.main != "undefined") {
+    if (data.weather[0].main === "Clouds") {
+      emoji = "fa-cloud";
+    } else if (data.weather[0].main === "Thunderstorm") {
+      emoji = "fa-bolt";
+    } else if (data.weather[0].main === "Drizzle") {
+      emoji = "fa-cloud-rain";
+    } else if (data.weather[0].main === "Rain") {
+      emoji = "fa-cloud-rain";
+    } else if (data.weather[0].main === "Snow") {
+      emoji = "fa-snow-flake";
+    } else if (data.weather[0].main === "Clear") {
+      emoji = "fa-sun";
+    } else {
+      emoji = "fa-smog";
+    }
+  } else {
+    return (
+      <div
+        style={{
+          backgroundColor: "#9c88ff",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "white",
+        }}
+      >
+        <div style={{ fontSize: "3rem", fontWeight: "bold" }}>Loading...</div>
+      </div>
+    );
+  }
+
+  let temp = (data.main.temp - 273.15).toFixed(2);
+  let temp_min = (data.main.temp_min - 273.15).toFixed(2);
+  let temp_max = (data.main.temp_max - 273.15).toFixed(2);
+
+  //Date
+  let d = new Date();
+  let year = d.getFullYear();
+  let date = d.getDate();
+  let month = d.toLocaleDateString("default", { month: "long" });
+  let day = d.toLocaleDateString("default", { weekday: "long" });
+
+  //Time
+  let time = d.toLocaleString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearch(input);
+    setInput("");
+  };
+
+  return (
+    <div className="main-box">
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-4">
+            <div className="card text-white text-center border-0">
+              <img
+                src={`https://source.unsplash.com/600x900/?weather/${data.weather[0].main}`}
+                className="card-img"
+                alt="background_image"
+              />
+              <div className="card-img-overlay">
+                <form onSubmit={handleSubmit} autoComplete="off">
+                  <div className="input-group mb-4 w-75 mx-auto">
+                    <input
+                      type="search"
+                      className="form-control"
+                      placeholder="Search City"
+                      aria-label="Search City"
+                      aria-describedby="basic-addon2"
+                      name="search"
+                      value={input}
+                      onChange={(e) => setInput(e.target.value)}
+                      required
+                    />
+                    <button
+                      type="submit"
+                      className="input-group-text"
+                      id="basic-addon2"
+                    >
+                      <i className="fas fa-search" />
+                    </button>
+                  </div>
+                </form>
+                <div className="opacity1">
+                  {/* bg-dark opacity1 py-3 */}
+                  <h2 className="card-title">{data.name}</h2>
+                  <p className="card-text lead">
+                    {day}, {month} {date}, {year}
+                    <br />
+                    {time}
+                  </p>
+                  <i className={`fas ${emoji} fa-4x`} />
+                  <h1 className="fw-bolder mb-5">{temp}&deg;C</h1>
+                  <p className="lead fw-bolder mb-0">{data.weather[0].main}</p>
+                  <p className="lead">
+                    {temp_min}&deg;C | {temp_max}&deg;C
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchWeather;
